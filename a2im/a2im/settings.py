2749bb711dfeea472303b223b6d9a51bef0b9702
Ace Burgess <burgess.ace@gmail.com>
2016-09-01 20:33:20 -0400

    Add more configurations

--- a2im/a2im/settings.py
+++ a2im/a2im/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'a2im.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'a2im',
+        'USER': 'root',
+        'PASSWORD': 'abc123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
